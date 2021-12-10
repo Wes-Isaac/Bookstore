c066a07d464ead998ea7fadee8b6988bdb83d4c2
unknown <yisehakmitiku@gmail.com>
2021-12-11 05:15:35 +0800

    Update style to look to the zeplin design

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,12 +6,26 @@ export default function Book({ book }) {
   const dispatch = useDispatch();
   return (
     <div className="list-item">
-      <p className="cate">{` ${book.category} `}</p>
-      <p className="book-title">{` ${book.title} `}</p>
       <div>
-        <button className="list-butt" type="button">Comment</button>
-        <button className="list-butt" type="button" onClick={() => dispatch(removeBook(book.item_id))}>Remove</button>
-        <button className="list-butt" type="button">Edit</button>
+        <p className="cate">{` ${book.category} `}</p>
+        <p className="book-title">{` ${book.title} `}</p>
+        <div>
+          <button className="list-butt" type="button">Comment</button>
+          <button className="list-butt" type="button" onClick={() => dispatch(removeBook(book.item_id))}>Remove</button>
+          <button className="list-butt" type="button">Edit</button>
+        </div>
+      </div>
+      <div className="progress">
+        <div className="oval-2" />
+        <div>
+          <h2>100%</h2>
+          <p>Completed</p>
+        </div>
+      </div>
+      <div className="info">
+        <p className="info-p">CURRENT CHAPTER</p>
+        <p>CHAPTER 15</p>
+        <button className="progress-button" type="button">UPDATE PROGRESS</button>
       </div>
     </div>
   );
